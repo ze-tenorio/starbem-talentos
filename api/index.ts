6993e4829f245a387cecfd35dcd2e469dc2cf032
José Tenório <jlptenorio@gmail.com>
2026-03-27 10:54:42 -0300

    vercel fix

---
+++ api/index.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express from "express";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { appRouter } from "../server/routers";
+import { createContext } from "../server/_core/context";
+
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use(
+  "/api/trpc",
+  createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
+
+export default app;
